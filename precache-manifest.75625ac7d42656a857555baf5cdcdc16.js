91dd06876bce3dba71dddf4297009301726e987a
Ezra Sharp <ezra.sharp@nice.kiwi>
2020-01-15 21:34:19 +1300

    Updates

---
+++ precache-manifest.75625ac7d42656a857555baf5cdcdc16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520655db93fbdf086e74c75ae30dbd31",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ba85a82129f127d6f9a8",
+    "url": "/monsters-rolodex/static/css/main.e6fcc595.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba85a82129f127d6f9a8",
+    "url": "/monsters-rolodex/static/js/main.14e16c49.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
